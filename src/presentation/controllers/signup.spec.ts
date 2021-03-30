d5203da55b2981310e37b8e9727a3f4937eb4581
Gustavo Barboza <gustavobarboza.oliveira@gmail.com>
2021-03-30 18:28:55 -0300

    test: ensure SignUpController return 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,4 +32,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
+  test('Should return 400 if no email is provided', () => {
+    const stu = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        // password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = stu.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
 });
